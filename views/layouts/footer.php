6f867570d1461dd5616982f629f106f7f83efc1f
Ian <ianjoesefhv@gmail.com>
2026-05-05 12:25:40 +0800

    fixed123

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,4 +1,4 @@
     <div id="toast-container" class="fixed bottom-5 right-5 z-50"></div>
-    <script src="/thrift_pos/public/assets/js/main.js"></script>
+    <script src="/assets/js/main.js"></script>
 </body>
 </html>
